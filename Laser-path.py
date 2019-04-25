a0fd1b7679c521bdb4f6a1dd952d0e2614050f25
lebed95 <49845366+lebed95@users.noreply.github.com>
2019-04-25 03:44:57 +0300

    Update Laser-path.py

--- Laser-path.py
+++ Laser-path.py
@@ -1,6 +1,5 @@
 import math
-#for point C(laser: C, A-C) and wall (A1,A2),D=A2-A1, A between A1-A2; B normal, new point C+2fD, f=(Ay-Cy+(Cx-Ax)By/Bx)/(Dy-ByDx/Bx)
-#new way (A, C+2fD-A)
+
 #Класс комнаты
 class Room:
     #[Переменная] Количество стен в комнате
@@ -35,6 +34,22 @@ def find_point(x1,y1,x2,y2,x3,y3,x4,y4):#точка пересечения от�
     coord[1]=((y3-y4)*coord[0]-(x3*y4-x4*y3))/(x4-x3)
     coord[2]=math.sqrt((x1-coord[0])**2+(y1-coord[1])**2)
     return coord
+def find_mir(Cx,Cy,Ax,Ay,A1x,A1y,A2x,A2y):
+    #for point C(laser: C, A-C) and wall (A1,A2),D=A2-A1, A between A1-A2; B normal, new point C+2fD, f=(Ay-Cy+(Cx-Ax)By/Bx)/(Dy-ByDx/Bx)
+    #new way (A, C+2fD-A)
+    coord=[0]*2
+    Dx=A2x-A1x
+    Dy=A2y-A1y
+    if(Dy==0):
+        Bx=0
+        By=1
+    else:
+        Bx=1
+        By=-Dx/Dy
+    f=(Ay*Bx-Cy*Bx+By*(Cx-Ax))/(Dy*Bx-By*Dx)
+    coord[0]=Cx+2*f*Dx
+    coord[1]=Cy+2*f*Dy
+    return coord
 
 def Laser_step(r,l1):
   l2=Laser
@@ -57,8 +72,18 @@ def Laser_step(r,l1):
   l1.Direction[1]=mas1[1]
   l2.Position[0]=mas1[0]
   l2.Position[1]=mas1[1]
+  l2.Direction[0]=mas1[0]
+  l2.Direction[1]=mas1[1]
   rmin=200
   for i in range (r.Number_of_walls):#ищем ближайшее пересечение с зеркалом
-        
+    if(cross(l1.Position[0],l1.Position[1],l1.Direction[0],l1.Direction[1],r.Nodes[i][0],r.Nodes[i][1],r.Nodes[int((i+1)%r.Number_of_walls)][0],r.Nodes[int((i+1)%r.Number_of_walls)][1])):
+      mas1=find_point(l1.Position[0],l1.Position[1],l1.Direction[0],l1.Direction[1],r.Nodes[i][0],r.Nodes[i][1],r.Nodes[int((i+1)%r.Number_of_walls)][0],r.Nodes[int((i+1)%r.Number_of_walls)][1]))
+      if(mas1[2]<rmin):
+            rmin=mas1[2]
+            l2.Position[0]=mas1[0]
+            l2.Position[1]=mas1[1] #запоминаем точку пересечения
+            mas2=find_mir(l1.Position[0],l1.Position[1],mas1[0],mas1[1],r.Nodes[i][0],r.Nodes[i][1],r.Nodes[int((i+1)%r.Number_of_walls)][0],r.Nodes[int((i+1)%r.Number_of_walls)][1]))
+            l2.Direction[0]=mas2[0]#запоминаем направление
+            l2.Direction[1]=mas2[1]
   return l2
 
